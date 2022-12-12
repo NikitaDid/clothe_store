92dfd7602b7308ed56c5a16f9516212e8ae590ab
Никитос <nikitadidenko222@gmail.com>
2022-12-12 21:56:53 +0300

    header and logo

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(reauest):
-    return HttpResponse('Hello. This is main page!')
+def index(request):
+    return render(request, 'index.html')

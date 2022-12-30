5d1d42517043af192eb35a85c1c5983ee1fbe94a
Никитос <nikitadidenko222@gmail.com>
2022-12-30 22:53:19 +0300

    admin tags links

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -24,12 +24,24 @@ class BlogCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category_link', 'publish_date', 'created_at']
+    list_display = ['id', 'title', 'category_link', 'tag_links', 'publish_date', 'created_at']
     list_display_links = ['id', 'title']
     list_filter = ['category', 'tags']
 
-    def category_link(self, instance):
+    def category_links(self, instance):
         url = reverse('admin:blog_blogcategory_change', args=[instance.category_id])
         return format_html(f"<a href={url}>{instance.category.name}</a>")
 
-    category_link.short_description = 'Категория'
+    category_links.short_description = 'Категория'
+
+    def tag_links(self, instance):
+        tags = instance.tags.all()
+        data = []
+        for tag in tags:
+            url = reverse('admin:blog_tag_change', args=[tag.id])
+            data.append(f"<a href='{url}'>{tag.name}</a>")
+        return format_html(str(data))
+
+
+    tag_links().short_description = 'Теги'
+
